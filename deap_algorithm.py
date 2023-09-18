3e0e2cbd8bbea50273e965e427ff5a0a2ab91691
matercomus <matek.kedzia@gmail.com>
2023-09-18 02:57:59 +0200

    added grid search

--- deap_algorithm.py
+++ deap_algorithm.py
@@ -23,10 +23,11 @@ from deap.tools.crossover import __all__ as all_crossovers
 from evoman.environment import Environment
 from demo_controller import player_controller
 
+
 def elitistRoulette(individuals, k, elitist_k, fit_attr="fitness"):
     """Select *k* individuals from the input *individuals* using *k*
-    spins of a roulette, and deterministically selecting the elitist_k best. 
-    The selection is made by looking only at the first fitness value of each individual. 
+    spins of a roulette, and deterministically selecting the elitist_k best.
+    The selection is made by looking only at the first fitness value of each individual.
     The list returned contains references to the input *individuals*.
 
     :param individuals: A list of individuals to select from.
@@ -48,7 +49,7 @@ def elitistRoulette(individuals, k, elitist_k, fit_attr="fitness"):
     s_inds = s_inds[elitist_k:]
     sum_fits = sum(getattr(ind, fit_attr).values[0] for ind in individuals)
     chosen = elitist_inds
-    for i in range(k-elitist_k):
+    for i in range(k - elitist_k):
         u = random.random() * sum_fits
         sum_ = 0
         for ind in s_inds:
@@ -58,15 +59,16 @@ def elitistRoulette(individuals, k, elitist_k, fit_attr="fitness"):
                 break
     return chosen
 
-class DEAP_Optimiser():
-    """ DEAP Optimiser class
 
-    Runs an evolutionary algorithm using the DEAP framework. Initialises using a 
+class DEAP_Optimiser:
+    """DEAP Optimiser class
+
+    Runs an evolutionary algorithm using the DEAP framework. Initialises using a
     JSON string that sets the configuration for the class, including hyperparameters
     of the algorithm, as well as methods of selection, mutation, and crossover.
 
     Contains the following methods:
-    
+
     :__init__(self, config): initialises the class, takes a JSON string with parameters
         for configuration.
     :set_env(self): configures EvoMan environment.
@@ -75,33 +77,37 @@ class DEAP_Optimiser():
     :optimise(self): main method, runs the whole algorithm for the number of generations set
         in the config, and returns or saves to disk resulting population and statistics.
     """
+
     def __init__(self, config):
         self.config = config
 
         # Calculates number of weights
-        n_inputs, n_outputs  = 20, 5
-        self.tot_neurons = self.config['h_neurons'] * (n_inputs + n_outputs + 1) + n_outputs
+        n_inputs, n_outputs = 20, 5
+        self.tot_neurons = (
+            self.config["h_neurons"] * (n_inputs + n_outputs + 1) + n_outputs
+        )
 
         # Set simulation Environment
         self.env = self.set_env()
-        
+
         # Set up DEAP toolbox
         self.toolbox = self.set_toolbox()
-      
+
     def set_env(self):
         """Initialises EvoMan simulation environment."""
-        if not os.path.exists(self.config['experiment_name']):
-            os.makedirs(self.config['experiment_name'])
+        if not os.path.exists(self.config["experiment_name"]):
+            os.makedirs(self.config["experiment_name"])
 
         env = Environment(
-            experiment_name=self.config['experiment_name'],
+            experiment_name=self.config["experiment_name"],
             enemies=[8],
             playermode="ai",
-            player_controller=player_controller(self.config['h_neurons']),
+            player_controller=player_controller(self.config["h_neurons"]),
             enemymode="static",
             level=2,
             speed="fastest",
-            visuals=False)
+            visuals=False,
+        )
         env.state_to_log()
         return env
 
@@ -109,47 +115,68 @@ class DEAP_Optimiser():
         """Initialises DEAP framework with specified parameters."""
         toolbox = base.Toolbox()
 
-        cx_methods =  {method_name:getattr(tools, method_name) for method_name in all_crossovers}
-        mut_methods = {method_name:getattr(tools, method_name) for method_name in all_mutations}
-        sel_methods = {method_name:getattr(tools, method_name) for method_name in all_selections}
-        sel_methods['elitistRoulette'] = elitistRoulette
+        cx_methods = {
+            method_name: getattr(tools, method_name) for method_name in all_crossovers
+        }
+        mut_methods = {
+            method_name: getattr(tools, method_name) for method_name in all_mutations
+        }
+        sel_methods = {
+            method_name: getattr(tools, method_name) for method_name in all_selections
+        }
+        sel_methods["elitistRoulette"] = elitistRoulette
 
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
         creator.create("Individual", list, fitness=creator.FitnessMax)
-        toolbox.register("attr_float", np.random.uniform,-1,1)
-        toolbox.register("individual", tools.initRepeat, creator.Individual, 
-                              toolbox.attr_float, n=self.tot_neurons)
+        toolbox.register("attr_float", np.random.uniform, -1, 1)
+        toolbox.register(
+            "individual",
+            tools.initRepeat,
+            creator.Individual,
+            toolbox.attr_float,
+            n=self.tot_neurons,
+        )
         toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-        toolbox.register("mate", cx_methods[self.config['cx_method']], **self.config['cx_kwargs'])
-        toolbox.register("mutate", mut_methods[self.config['mut_method']], **self.config['mut_kwargs'])
-        toolbox.register("select", sel_methods[self.config['sel_method']], **self.config['sel_kwargs'])
+        toolbox.register(
+            "mate", cx_methods[self.config["cx_method"]], **self.config["cx_kwargs"]
+        )
+        toolbox.register(
+            "mutate",
+            mut_methods[self.config["mut_method"]],
+            **self.config["mut_kwargs"],
+        )
+        toolbox.register(
+            "select",
+            sel_methods[self.config["sel_method"]],
+            **self.config["sel_kwargs"],
+        )
         toolbox.register("evaluate", self.evaluate)
         return toolbox
-        
+
     def evaluate(self, individual):
-        """ Run EvoMan and return fitness of individual.
-        
+        """Run EvoMan and return fitness of individual.
+
         Takes an individual (array of weights, in the case of a NN), and runs a simulation of
         an EvoMan game with that individual as a controller, returning the fitness of the individual
         as a result."""
-        f,p,e,t = self.env.play(pcont=np.array(individual))
+        f, p, e, t = self.env.play(pcont=np.array(individual))
         return f
 
     def optimise(self):
-        """ Train an algorithm to play EvoMan by using the DEAP framework.
-        
+        """Train an algorithm to play EvoMan by using the DEAP framework.
+
         Runs for several generations, selecting and mating the best algorithms according to the
-        methods specified to find an optimised solution. Returns the last population of algorithms 
+        methods specified to find an optimised solution. Returns the last population of algorithms
         as a result.
         """
-        pop = self.toolbox.population(n=self.config['population_size'])
+        pop = self.toolbox.population(n=self.config["population_size"])
 
         # Evaluate the entire population
         fitnesses = map(self.toolbox.evaluate, pop)
         for ind, fit in zip(pop, fitnesses):
             ind.fitness.values = [fit]
 
-        for g in range(self.config['n_generations']):
+        for g in range(self.config["n_generations"]):
             # Select the next generation individuals
             offspring = self.toolbox.select(pop, len(pop))
             # Clone the selected individuals
@@ -157,14 +184,14 @@ class DEAP_Optimiser():
 
             # Apply crossover and mutation on the offspring
             for child1, child2 in zip(offspring[::2], offspring[1::2]):
-                if random.random() < self.config['cx_probability']:
+                if random.random() < self.config["cx_probability"]:
                     self.toolbox.mate(child1, child2)
                     del child1.fitness.values
                     del child2.fitness.values
 
             # Mutate individuals
             for mutant in offspring:
-                if random.random() < self.config['mut_probability']:
+                if random.random() < self.config["mut_probability"]:
                     self.toolbox.mutate(mutant)
                     del mutant.fitness.values
 
@@ -180,8 +207,11 @@ class DEAP_Optimiser():
         return pop
 
     def log_gen(self, n_gen, historical_results):
-        print(f'Mean Fitness for Generation {n_gen}: {np.mean(historical_results)}.')
-        with open(f'./{self.config["experiment_name"]}/fitnesses.json', 'a') as out_file:
-            out_file.write(json.dumps({'generation':n_gen, 'fitnesses': historical_results}))
+        print(f"Mean Fitness for Generation {n_gen}: {np.mean(historical_results)}.")
+        with open(
+            f'./{self.config["experiment_name"]}/fitnesses.json', "a"
+        ) as out_file:
+            out_file.write(
+                json.dumps({"generation": n_gen, "fitnesses": historical_results})
+            )
             out_file.write("\n")
-
--- run_experiments_cx_comaprison.py
+++ run_experiments_cx_comaprison.py
@@ -6,6 +6,7 @@ loading or generating configurations as needed.
 import json
 import itertools
 
+from tqdm import tqdm
 import numpy as np
 
 # DEAP optimiser
@@ -22,12 +23,9 @@ exp_template = config["experiments"][0]
 # Define the methods and their possible kwargs values
 methods = [
     # Working
-    {"name": "cxOnePoint", "kwargs": {}},
-    {"name": "cxTwoPoint", "kwargs": {}},
-    {
-        "name": "cxUniform",
-        "kwargs": {"indpb": list(np.arange(0.1, 1.0, 0.1))},
-    },
+    # {"name": "cxOnePoint", "kwargs": {}},
+    # {"name": "cxTwoPoint", "kwargs": {}},
+    {"name": "cxUniform", "kwargs": {"indpb": list(np.arange(0.1, 1.0, 0.1))}},
     {"name": "cxBlend", "kwargs": {"alpha": list(np.arange(0.1, 1.0, 0.1))}},
     {"name": "cxSimulatedBinary", "kwargs": {"eta": list(range(1, 15, 1))}},
     # Not working
@@ -40,6 +38,13 @@ methods = [
     # {"name": "cxESTwoPoint", "kwargs": {}},
 ]
 
+# Calculate the total number of combinations for all methods
+total_combinations = sum(
+    len(list(itertools.product(*method["kwargs"].values()))) for method in methods
+)
+
+# Create a progress bar
+pbar = tqdm(total=total_combinations, desc="Total progress")
 
 # Iterate over each method
 for method in methods:
@@ -47,7 +52,10 @@ for method in methods:
     kwargs = method["kwargs"]
 
     # Generate all combinations of kwargs values
-    combinations = list(itertools.product(*kwargs.values()))
+    combinations = [
+        dict(zip(kwargs.keys(), values))
+        for values in itertools.product(*kwargs.values())
+    ]
 
     # Iterate over each combination
     for combination in combinations:
@@ -55,11 +63,22 @@ for method in methods:
         exp = exp_template.copy()
 
         # Update the experiment parameters
-        exp["experiment_name"] = f"crossover_exp_{method['name']}_{combination}"
+        arg_names = (
+            "_".join([f"{k}_{v}" for k, v in combination.items()])
+            if combination
+            else ""
+        )
+        exp["experiment_name"] = f"crossover_exp_{method['name']}_{arg_names}"
         exp["cx_method"] = method["name"]
-        exp["cx_kwargs"] = dict(zip(kwargs.keys(), combination))
+        exp["cx_kwargs"] = combination
 
         # Run the experiment
         print(json.dumps(exp, indent=4))
         optimiser = DEAP_Optimiser(exp)
         optimiser.optimise()
+
+        # Update the progress bar
+        pbar.update()
+
+# Close the progress bar
+pbar.close()
