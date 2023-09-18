832dd22cdb9ccbc446f5da2bbb4a845a500d01a8
matercomus <matek.kedzia@gmail.com>
2023-09-18 04:59:25 +0200

    improved find_best and set it to create result josn file

--- find_best.py
+++ find_best.py
@@ -1,9 +1,6 @@
 import os
 import json
 import statistics
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 
 
 class Utils:
@@ -12,87 +9,55 @@ class Utils:
         self.prefix = prefix
 
     def process_directories(self):
-        # Initialize an empty dictionary to store the results
         results = {}
-
         for root, dirs, files in os.walk(self.start_dir):
             for name in dirs:
                 if name.startswith(self.prefix):
-                    # Construct the full path to the fitnesses.json file
-                    fitness_file = os.path.join(root, name, "fitnesses.json")
-
-                    # Check if the file exists
-                    if os.path.isfile(fitness_file):
-                        with open(fitness_file, "r") as f:
-                            last_line = f.readlines()[-1]
-                            data = json.loads(last_line)
-
-                            # Add the directory name to the dictionary
-                            data["name"] = name
-
-                            # Calculate the average fitness
-                            avg_fitness = statistics.mean(data["fitnesses"])
-                            data["avg_fitness"] = avg_fitness
-
-                            # Calculate the maximum fitness
-                            max_fitness = max(data["fitnesses"])
-                            data["max_fitness"] = max_fitness
-
-                            # Calculate the number of individuals
-                            n_individuals = len(data["fitnesses"])
-                            data["n_individuals"] = n_individuals
-
-                            # Store the results in the dictionary
-                            results[name] = data
-
+                    data = self.process_directory(root, name)
+                    if data is not None:
+                        results[name] = data
         return results
 
-    def print_results(self, results):
-        for name, data in results.items():
-            print(
-                f"Name: {data['name']}, Generation: {data['generation']}, Average Fitness: {data['avg_fitness']}, Max Fitness: {data['max_fitness']}, Number of Individuals: {data['n_individuals']}"
-            )
+    def process_directory(self, root, name):
+        fitness_file = os.path.join(root, name, "fitnesses.json")
+        if os.path.isfile(fitness_file):
+            with open(fitness_file, "r") as f:
+                last_line = f.readlines()[-1]
+                data = json.loads(last_line)
+                data.update(self.calculate_statistics(data, name))
+                kwargs_dict = self.extract_kwargs(name)
+                if kwargs_dict is not None:
+                    data["kwargs"] = kwargs_dict
+                return data
+        return None
 
-    def create_bar_chart(self, results):
-        # Sort the results in descending order by average fitness and limit to top 10
-        sorted_results = sorted(
-            results.items(), key=lambda x: x[1]["avg_fitness"], reverse=True
-        )[:10]
+    def calculate_statistics(self, data, name):
+        fitnesses = data["fitnesses"]
+        return {
+            "name": data.get("name", name),
+            "avg_fitness": statistics.mean(fitnesses),
+            "max_fitness": max(fitnesses),
+            "n_individuals": len(fitnesses),
+        }
 
-        # Prepare data for the bar chart
-        names = [data["name"] for name, data in sorted_results]
-        avg_fitnesses = [data["avg_fitness"] for name, data in sorted_results]
-        max_fitnesses = [data["max_fitness"] for name, data in sorted_results]
-
-        # Create a DataFrame from the results
-        df = pd.DataFrame(
-            list(zip(names, avg_fitnesses, max_fitnesses)),
-            columns=["Name", "Average Fitness", "Max Fitness"],
+    def extract_kwargs(self, name):
+        parts = name.split("_")
+        cx_index = next(
+            (i for i, part in enumerate(parts) if part.startswith("cx")), None
         )
+        if cx_index is not None and (len(parts) - cx_index - 1) % 2 == 0:
+            return {
+                parts[i]: float(parts[i + 1])
+                for i in range(cx_index + 1, len(parts), 2)
+            }
+        return None
 
-        # Melt the DataFrame to make it suitable for seaborn
-        df_melted = pd.melt(
-            df, id_vars="Name", var_name="Fitness Type", value_name="Fitness"
-        )
-
-        # Create a bar chart using seaborn
-        sns.set_theme(style="whitegrid")
-        plt.figure(figsize=(20, 10))  # Adjust size here
-
-        chart = sns.barplot(x="Name", y="Fitness", hue="Fitness Type", data=df_melted)
-
-        chart.set_xticklabels(chart.get_xticklabels(), rotation=90)
-
-        plt.title("Average and Max Fitness by Name")
-
-        plt.tight_layout()
-
-        # Save the figure as a PNG file
-        plt.savefig("average_max_fitness.png")
+    def save_results(self, results, filename):
+        with open(filename, "w") as f:
+            json.dump(results, f)
 
 
 # Usage:
 utils = Utils(".", "crossover_exp_")
 results = utils.process_directories()
-utils.print_results(results)
-utils.create_bar_chart(results)
+utils.save_results(results, "results.json")
