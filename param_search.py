c2cb7ca865b0f16323a0c9c0a25f0529c37b905c
matercomus <matek.kedzia@gmail.com>
2023-09-27 17:03:46 +0200

    swithed param_search to not use cofig object but dict instead

--- param_search.py
+++ param_search.py
@@ -5,45 +5,26 @@ import numpy as np
 
 # Define your objective function
 def objective_function(x, config_keys, config):
-    # Update the config object with the parameters from x
+    # Update the config dictionary with the parameters from x
     for i, key in enumerate(config_keys):
-        if key.endswith("_bounds"):
-            continue  # Skip the bounds entries
-        setattr(config, key, x[i])
+        config[key] = x[i]
 
     # Create a DEAP_Optimiser object and run the optimise method
     optimiser = DEAP_Optimiser(config)
-
-    # Initialize best fitness at generation 10 to negative infinity
-    best_gen_10 = -np.inf
-
-    for gen in range(config.n_generations):
-        fitness = optimiser.optimise()
-
-        # Check if we're at generation 10
-        if gen == 10:
-            # If fitness is worse than best_gen_10, stop early and return fitness
-            if fitness < best_gen_10:
-                return -fitness
-
-            # Otherwise, update best_gen_10 and continue
-            best_gen_10 = fitness
+    max_fitness = optimiser.optimise()
 
     # Since we're using dual_annealing which minimizes the objective function,
     # we return the negative of max_fitness to maximize it.
-    return -fitness
+    return -max_fitness
 
 
 # Read the JSON file
 with open("config.json", "r") as f:
-    config_dict = json.load(f)
-
-# Create a DEAPConfig object from the dictionary
-config = DEAPConfig(config_dict)
+    config = json.load(f)
 
 # Precompile the list of keys and bounds
-config_keys = [key for key in config.__dict__.keys() if not key.endswith("_bounds")]
-bounds = [getattr(config, key + "_bounds") for key in config_keys]
+config_keys = [key for key in config.keys() if not key.endswith("_bounds")]
+bounds = [config[key + "_bounds"] for key in config_keys]
 
 
 # Define a local function for the objective
