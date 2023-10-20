e650c3ec3a2d8577875f45aa59fd579c2d55df5b
jcardonaruiz <joaquincardonaruiz@gmail.com>
2023-10-20 15:08:11 +0200

    Experiments for assignment 2 completed

--- graphs.py
+++ graphs.py
@@ -13,14 +13,14 @@ def load_data_lineplots(models, models_dirname, enemies, runs):
         for e in enemies:
             data[m][e] = {}
             for r in runs:
-                with open(f'experiment_results/{m}/e{e}/experiment_{models_dirname[m]}_e{e}_r{r}/fitnesses.json','r') as file:
+                with open(f'experiment_results_generalist/{m}/e{e}/{models_dirname[m]}_e_{e}_r{r}/fitnesses.json','r') as file:
                     fit_data = []
                     while True:
                         try:
                             fit_data.append(json.loads(file.readline()))
                         except:
                             break
-                with open(f'experiment_results/{m}/e{e}/experiment_{models_dirname[m]}_e{e}_r{r}/results.json', 'r') as file:
+                with open(f'experiment_results_generalist/{m}/e{e}/{models_dirname[m]}_e_{e}_r{r}/results.json', 'r') as file:
                     res_data = json.loads(file.read())
                 data[m][e][r] = {'means':[np.mean(gen['fitnesses']) for gen in fit_data]+[res_data['mean']],
                                  'max':  [np.max(gen['fitnesses']) for gen in fit_data]+[res_data['max']]}
@@ -51,11 +51,12 @@ def load_data_lineplots(models, models_dirname, enemies, runs):
 def lineplots():
     #sns.set_theme(font='Computer Modern Serif')
     models = ["Model 1", "Model 2"]
-    models_colours = {"Model 1": "maroon", "Model 2": "lightsteelblue"}
-    models_dirname = {"Model 1": "fs", "Model 2": "nofs"}
-    enemies = ["2","5","7"]
+    models_colours = {"Model 1": "lightsteelblue", "Model 2": "midnightblue"}
+    models_dirname = {"Model 1": "generalist_naa", "Model 2": "generalist_aa"}
+    enemies = ["set_1","set_2"]
+    enemy_names = {"set_1":'[2,5]',"set_2":'[2,4,6,7]'}
     runs = [str(i) for i in range(10)]
-    generations = list(range(51))
+    generations = list(range(76))
     data = load_data_lineplots(models, models_dirname, enemies, runs)
 
     for e in enemies:
@@ -69,12 +70,12 @@ def lineplots():
             plt.fill_between(generations, data_xgens_this_model['avg']-data_xgens_this_model['std_avg'], data_xgens_this_model['avg']+data_xgens_this_model['std_avg'], color = models_colours[m], alpha=0.3, label='_nolegend_')
             plt.fill_between(generations, data_xgens_this_model['max']-data_xgens_this_model['std_max'], data_xgens_this_model['max']+data_xgens_this_model['std_max'], color = models_colours[m], alpha=0.3, label='_nolegend_')
         plt.grid(zorder=0, color='lightgrey', alpha=0.7)
-        plt.xticks(np.arange(0,51,5))
+        plt.xticks(np.arange(0,76,5))
         plt.yticks(np.arange(0,100,5))
         plt.xlabel("Generations")
         plt.ylabel("Fitness")
-        plt.legend(["Model 1 (Fit-Sharing) mean", "Model 1 (Fit-Sharing) max", "Model 2 (No Fit-Sharing) mean", "Model 2 (No Fit-Sharing) max", "Model 2 (No Fit-Sharing) mean", "Model 2 (No Fit-Sharing) max"], loc='lower right')
-        plt.title(f'Fitness over Generations for Enemy {e}')
+        plt.legend(["Model 1 (No Adaptive α) mean", "Model 1 (No Adaptive α) max", "Model 2 (Adaptive α) mean", "Model 2 (Adaptive α) max"], loc='lower right')
+        plt.title(f'Fitness over Generations for Enemies {enemy_names[e]}')
         plt.show()
 
 def param_tuning_graphs():
@@ -114,14 +115,14 @@ def load_data_boxplot(models, models_dirname, enemies, runs):
         for e in enemies:
             data[m][e] = {}
             for r in runs:
-                with open(f'experiment_results/{m}/e{e}/experiment_{models_dirname[m]}_e{e}_r{r}/fitnesses.json','r') as file:
+                with open(f'experiment_results_generalist/{m}/e{e}/{models_dirname[m]}_e_{e}_r{r}/fitnesses.json','r') as file:
                     fit_data = []
                     while True:
                         try:
                             fit_data.append(json.loads(file.readline()))
                         except:
                             break
-                with open(f'experiment_results/{m}/e{e}/experiment_{models_dirname[m]}_e{e}_r{r}/results.json', 'r') as file:
+                with open(f'experiment_results_generalist/{m}/e{e}/{models_dirname[m]}_e_{e}_r{r}/results.json', 'r') as file:
                     res_data = json.loads(file.read())
                 data[m][e][r] = res_data['best']
 
@@ -134,14 +135,14 @@ def load_data_diversity(models, models_dirname, enemies, runs):
         for e in enemies:
             data[m][e] = {}
             for r in runs:
-                with open(f'experiment_results/{m}/e{e}/experiment_{models_dirname[m]}_e{e}_r{r}/fitnesses.json','r') as file:
+                with open(f'experiment_results_generalist/{m}/e{e}/{models_dirname[m]}_e_{e}_r{r}/fitnesses.json','r') as file:
                     fit_data = []
                     while True:
                         try:
                             fit_data.append(json.loads(file.readline()))
                         except:
                             break
-                with open(f'experiment_results/{m}/e{e}/experiment_{models_dirname[m]}_e{e}_r{r}/results.json', 'r') as file:
+                with open(f'experiment_results_generalist/{m}/e{e}/{models_dirname[m]}_e_{e}_r{r}/results.json', 'r') as file:
                     res_data = json.loads(file.read())
                 data[m][e][r] = {'shannon': [gen['diversity_shannon'] for gen in fit_data]}
 
@@ -167,21 +168,22 @@ def load_data_diversity(models, models_dirname, enemies, runs):
 
 def diversity():
     models = ["Model 1", "Model 2"]
-    models_colours = {"Model 1": "maroon", "Model 2": "lightsteelblue"}
-    enemies_dashes = {"2": "-", "5": "--", '7': ':'}
-    models_dirname = {"Model 1": "fs", "Model 2": "nofs"}
-    enemies = ["2","5","7"]
+    models_colours = {"Model 1": "lightsteelblue", "Model 2": "midnightblue"}
+    enemies_dashes = {"set_1": "-", "set_2": "--"}
+    models_dirname = {"Model 1": "generalist_naa", "Model 2": "generalist_aa"}
+    enemy_names = {"set_1":'[2,5]',"set_2":'[2,4,6,7]'}
+    enemies = ["set_1","set_2"]
     runs = [str(i) for i in range(10)]
-    generations = list(range(50))
+    generations = list(range(75))
     data = load_data_diversity(models, models_dirname, enemies, runs)
     legends = []
     for e in enemies:
         for m in models:
             data_xgens_this_model = np.array([np.mean(data[e][m][f'{gen}']['shannon']) for gen in generations])
-            legends.append(f'{m} | Enemy {e}')
+            legends.append(f'{m} | Enemy {enemy_names[e]}')
             plt.plot(generations, data_xgens_this_model, enemies_dashes[e], color = models_colours[m])
     plt.grid(zorder=0, color='lightgrey', alpha=0.7)
-    plt.xticks(np.arange(0,50,5))
+    plt.xticks(np.arange(0,75,5))
     plt.yticks(np.arange(0,3.25,0.25))
     plt.xlabel("Generations")
     plt.ylabel("Diversity")
@@ -189,55 +191,82 @@ def diversity():
     plt.title(f'Diversity by Model and Enemy')
     plt.show()
 
-def eval_data():
+def eval_table_data():
     models = ["Model 1", "Model 2"]
-    models_dirname = {"Model 1": "fs", "Model 2": "nofs"}
-    enemies = ["2","5","7"]
+    models_dirname = {"Model 1": "generalist_naa", "Model 2": "generalist_aa"}
+    enemies = ["set_1","set_2"]
     runs = [str(i) for i in range(10)]
     data = load_data_boxplot(models, models_dirname,enemies,runs)
     results = {}
     for m in models:
         for e in enemies:
-            results[f'run_{m}_e{e}'] = []
+            results[f'run_{m}_e_{e}'] = []
             for r in runs:
                 gains = []
-                ev = Evaluator({'run_name': f'run_{m}_e{e}_r{r}', 
-                                'enemy': int(e),
-                                'h_neurons': 30,
+                ev = Evaluator({'run_name': f'run_{m}_e_{e}_r{r}', 
+                                'enemies': [1,2,3,4,5,6,7,8],
+                                'h_neurons': 10,
                                 'weights': data[m][e][r]})
                 for i in range(5):
-                    f = ev.run()
-                    gains.append(f)
-                results[f'run_{m}_e{e}'].append(np.mean(gains))
+                    g = ev.run()
+                    gains.append(g)
+                results[f'run_{m}_e_{e}'].append(np.mean(gains))
+    with open('boxplot_results.json', 'a') as file:
+        file.write(json.dumps(results))
+
+def table():
+    pass
+
+def eval_data():
+    models = ["Model 1", "Model 2"]
+    models_dirname = {"Model 1": "generalist_naa", "Model 2": "generalist_aa"}
+    enemies = ["set_1","set_2"]
+    runs = [str(i) for i in range(10)]
+    data = load_data_boxplot(models, models_dirname,enemies,runs)
+    results = {}
+    for m in models:
+        for e in enemies:
+            results[f'run_{m}_e_{e}'] = []
+            for r in runs:
+                gains = []
+                ev = Evaluator({'run_name': f'run_{m}_e_{e}_r{r}', 
+                                'enemies': [1,2,3,4,5,6,7,8],
+                                'h_neurons': 10,
+                                'weights': data[m][e][r]})
+                for i in range(5):
+                    g = ev.run()
+                    gains.append(g)
+                results[f'run_{m}_e_{e}'].append(np.mean(gains))
     with open('boxplot_results.json', 'a') as file:
         file.write(json.dumps(results))
                 
 
 def boxplot():
     sns.set_style(style='white')
-    models_colours = {"Model 1": "maroon", "Model 2": "lightsteelblue"}
+    models_colours = {"Model 1": "lightsteelblue", "Model 2": "midnightblue"}
     with open('boxplot_results.json','r') as file:
         data = json.loads(file.read())
     df = pd.DataFrame(data)
-    df = df[['run_Model 1_e2', 'run_Model 2_e2', 'run_Model 1_e5', 'run_Model 2_e5', 'run_Model 1_e7', 'run_Model 2_e7']]
-    df = df.rename(columns={'run_Model 1_e2': 'Model 1 \n(Fitness-Sharing) \n Enemy 2',
-                            'run_Model 1_e5': 'Model 1 \n(Fitness-Sharing) \n Enemy 5',
-                            'run_Model 1_e7': 'Model 1 \n(Fitness-Sharing) \n Enemy 7',
-                            'run_Model 2_e2': 'Model 2 \n(No Fitness-Sharing) \n Enemy 2',
-                            'run_Model 2_e5': 'Model 2 \n(No Fitness-Sharing) \n Enemy 5',
-                            'run_Model 2_e7': 'Model 2 \n(No Fitness-Sharing) \n Enemy 7'})
+    df = df[['run_Model 1_e_set_1', 'run_Model 2_e_set_1', 'run_Model 1_e_set_2', 'run_Model 2_e_set_2']]
+    df = df.rename(columns={'run_Model 1_e_set_1': 'Model 1 \n(No Adaptive α) \n Trained on enemies [2,5]',
+                            'run_Model 1_e_set_2': 'Model 1 \n(No Adaptive α) \n Trained on enemies [2,4,6,7]',
+                            'run_Model 2_e_set_1': 'Model 2 \n(Adaptive α) \n Trained on enemies [2,5]',
+                            'run_Model 2_e_set_2': 'Model 2 \n(Adaptive α) \n Trained on enemies [2,4,6,7]'})
     df = df.melt() 
     df['model'] = df['variable'].str[:7]
     df = df.rename(columns={'variable': 'Experiment', 'value': 'Gain', 'model': 'Model'})
     plt.grid(color='lightgrey')
     plt.xticks(fontsize=10)
-    plt.yticks(np.arange(0,100,5))
+    plt.yticks(np.arange(-800,800,25))
     plt.ylabel('Individual Gain')
     plt.xlabel('',color="white")
-    plt.title('Gain of Best Individuals Across 10 Rounds')
+    plt.title('Gain of Best Individuals Across 10 Rounds\nAveraged across 5 evaluations across all 8 enemies')
     sns.boxplot(x=df['Experiment'], y=df['Gain'],hue=df['Model'],palette=list(models_colours.values()), medianprops=dict(color="darkgrey"))
     plt.show()
 
-diversity()
-boxplot()
-lineplots()
+#diversity()
+#boxplot()
+#lineplots()
+#eval_data()
+
+table()
